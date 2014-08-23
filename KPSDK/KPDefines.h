7ebeef748775c441838b32c6bc1f6d048d4334c5
Vincent Chen <vince78718@gmail.com>
2014-08-23 08:35:24 +0800

    add required string defines

---
+++ KPSDK/KPDefines.h
@@ -0,0 +1,15 @@
+//
+//  KPDefines.h
+//  KPSDK
+//
+//  Created by vincent on 2014/08/23.
+//  Copyright (c) 2014 Vincent Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - API
+
+extern NSString *const kKPAPIScheme;    //! @constant protocol of KPAPI
+extern NSString *const kKPAPIHost;      //! @constant host of KPAPI
+extern NSString *const kKPAPIVersion;   //! @constant default version of KPAPI
---
+++ KPSDK/KPDefines.m
@@ -0,0 +1,15 @@
+//
+//  KPDefines.m
+//  KPSDK
+//
+//  Created by vincent on 2014/08/23.
+//  Copyright (c) 2014 Vincent Chen. All rights reserved.
+//
+
+#import "KPDefines.h"
+
+#pragma mark - API
+
+NSString *const kKPAPIScheme = @"http";
+NSString *const kKPAPIHost = @"api.kptaipei.tw";
+NSString *const kKPAPIVersion = @"v1";

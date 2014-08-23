5836e9b209155d74bd7da56b14835e82222f90df
Vincent Chen <vince78718@gmail.com>
2014-08-23 09:41:47 +0800

    setup default value for KPObject.

--- KPSDK/KPObject.m
+++ KPSDK/KPObject.m
@@ -8,6 +8,22 @@
 
 #import "KPObject.h"
 
+@interface KPObject ()
+
+@property (readwrite) NSString *kpClassName;
+
+@end
+
 @implementation KPObject
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.kpClassName = @"Object";
+        self.objectId = nil;
+    }
+    return self;
+}
 
 @end

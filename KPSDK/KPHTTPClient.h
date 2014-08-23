d494a987019d3657b2087bf0b99c584b09e83cf4
Vincent Chen <vince78718@gmail.com>
2014-08-23 17:12:08 +0800

    Adding implementations that fetches category and articles.

--- KPSDK/KPHTTPClient.h
+++ KPSDK/KPHTTPClient.h
@@ -8,6 +8,9 @@
 
 #import "AFNetworking.h"
 
+typedef void (^KPHTTPClientSuccess)(NSURLSessionDataTask *task, id responseObject);
+typedef void (^KPHTTPClientFailure)(NSURLSessionDataTask *task, NSError *error);
+
 @interface KPHTTPClient : AFHTTPSessionManager
 
 #pragma mark - Client Lifecycle
@@ -18,4 +21,28 @@
 - (void)setAPIKey:(NSString *)apiKey;
 - (NSString *)getAPIKey;
 
+#pragma mark - Category
+
+/*!
+ Get category list
+ @param success success callback, will return the operation and category array for response object if has any.
+ @param failur failure callback, will return the operation and the error if has any.
+ */
+- (void)getCategoriesWithSuccess:(KPHTTPClientSuccess)success failure:(KPHTTPClientFailure)failure;
+
+#pragma mark - Article List
+
+/*!
+ Get article list by category id
+ 
+ @param objectId as NSString, the id of category.
+ @param page as NSUInteger, default is 0.
+ @param pageSize as NSUInteger, default is 100, max is 500, giving 0 to got default 100 records.
+ @param success success callback, will return the operation and category array for response object if has any.
+ @param failur failure callback, will return the operation and the error if has any.
+ */
+- (void)getArticlesByCategoryId:(NSString *)objectId page:(NSInteger)page pageSize:(NSInteger)pageSize success:(KPHTTPClientSuccess)success failure:(KPHTTPClientFailure)failure;
+- (void)getArticlesByCategoryId:(NSString *)objectId page:(NSInteger)page success:(KPHTTPClientSuccess)success failure:(KPHTTPClientFailure)failure;
+- (void)getArticlesByCategoryId:(NSString *)objectId pageSize:(NSInteger)pageSize success:(KPHTTPClientSuccess)success failure:(KPHTTPClientFailure)failure;
+- (void)getArticlesByCategoryId:(NSString *)objectId success:(KPHTTPClientSuccess)success failure:(KPHTTPClientFailure)failure;
 @end
--- KPSDK/KPHTTPClient.m
+++ KPSDK/KPHTTPClient.m
@@ -9,7 +9,16 @@
 #import "KPHTTPClient.h"
 #import "KPDefines.h"
 
+#import "KPCategory.h"
+#import "KPArticle.h"
+
 NSString * const kKPRequestParameterAccessToken = @"accessToken";
+NSString * const kKPRequestParameterPage = @"page";
+NSString * const kKPRequestParameterPageSize = @"page_size";
+
+NSString * const kKPRequestPathCategory = @"category";
+NSString * const kKPRequestPathAlbums = @"albums";
+NSString * const kKPRequestPathVideos = @"videos";
 
 @interface KPHTTPClient ()
 
@@ -71,6 +80,97 @@ NSString * const kKPRequestParameterAccessToken = @"accessToken";
         // By Default, response serializer is set to JSON, no need to setup here
     }
     return self;
+}
+
+#pragma mark - GET Request
+
+- (NSURLSessionDataTask *)GET:(NSString *)URLString parameters:(id)parameters success:(void (^)(NSURLSessionDataTask *, id))success failure:(void (^)(NSURLSessionDataTask *, NSError *))failure
+{
+    if ( ! [self getAPIKey]) {
+        
+        @throw([NSException exceptionWithName:@"kKPAPIKeyNotSetException" reason:@"API key is not been set" userInfo:nil]);
+    }
+    
+    NSMutableDictionary *completeParameter = [NSMutableDictionary dictionaryWithDictionary:self.baseParameter];
+    
+    if (parameters && [parameters isKindOfClass:[NSDictionary class]]) {
+        
+        [completeParameter addEntriesFromDictionary:parameters];
+    }
+    
+    return [super GET:URLString parameters:completeParameter success:success failure:failure];
+}
+
+#pragma mark - Category
+
+- (void)getCategoriesWithSuccess:(KPHTTPClientSuccess)success failure:(KPHTTPClientFailure)failure
+{
+    [self GET:kKPRequestPathCategory parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        
+        if (responseObject) {
+            
+            NSArray *data = responseObject[@"data"];
+            __block NSMutableArray *categoryArray = [NSMutableArray array];
+            
+            [data enumerateObjectsUsingBlock:^(NSDictionary *dataDictionary, NSUInteger idx, BOOL *stop) {
+                KPCategory *category = [KPCategory objectWithDictionary:dataDictionary];
+                
+                [categoryArray addObject:category];
+                
+            }];
+            
+            success(task, categoryArray);
+        }
+        
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        failure(task, error);
+    }];
+}
+
+#pragma mark - Article List
+
+- (void)getArticlesByCategoryId:(NSString *)objectId page:(NSInteger)page pageSize:(NSInteger)pageSize success:(KPHTTPClientSuccess)success failure:(KPHTTPClientFailure)failure
+{
+    if ( ! objectId) {
+        @throw([NSException exceptionWithName:@"kKPRequestCategoryIdNotSetException" reason:@"category id shoud be set for this request." userInfo:nil]);
+    }
+    
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    
+    if (page > 0) {
+        [parameters setObject:@(page) forKey:kKPRequestParameterPage];
+    }
+    
+    if (pageSize > 0 && pageSize <= 500) {
+        [parameters setObject:@(pageSize) forKey:kKPRequestParameterPage];
+    } else if (pageSize <= 0) {
+        [parameters setObject:@(100) forKey:kKPRequestParameterPage];
+    } else {
+        [parameters setObject:@(500) forKey:kKPRequestParameterPage];
+    }
+    
+    NSString *path = [NSString stringWithFormat:@"%@/%@", kKPRequestPathCategory, objectId];
+    
+    [self GET:path parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        
+        if (responseObject) {
+            
+            NSArray *data = responseObject[@"data"];
+            __block NSMutableArray *articlesArray = [NSMutableArray array];
+            
+            [data enumerateObjectsUsingBlock:^(NSDictionary *dataDictionary, NSUInteger idx, BOOL *stop) {
+                KPArticle *article = [KPArticle objectWithDictionary:dataDictionary];
+                
+                [articlesArray addObject:article];
+                
+            }];
+            
+            success(task, articlesArray);
+        }
+        
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        failure(task, error);
+    }];
 }
 
 @end

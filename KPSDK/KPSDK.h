d3655a15cbbb6410b313d23c028d392ffdd9aeac
Vincent Chen <vince78718@gmail.com>
2014-08-23 09:41:11 +0800

    add the initializer of the API

--- KPSDK/KPSDK.h
+++ KPSDK/KPSDK.h
@@ -7,3 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
+
+#import "KPDefines.h"
+
+#import "KPHTTPClient.h"
+
+#import "KPObject.h"
+
+@interface KPSDK : NSObject
+
+#pragma - KPAPI
+
+/** @name Connection with KPAPI */
+
+/*!
+ [Required!] Set the API Key of your appilcation.
+ @param apiKey The API Key given by http://unlimited.kptaipei.tw/
+ */
++ (void)setAPIKey:(NSString *)apiKey;
+/*!
+ get current API Key
+ */
++ (NSString *)getAPIKey;
+
+@end
